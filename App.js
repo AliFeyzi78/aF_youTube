ef553ba209447fed855b61004907833b173199bc
AliFeyzi78 <zeron1g2k@gmail.com>
2020-10-29 21:32:29 +0200

    ended

--- App.js
+++ App.js
@@ -1,19 +1,66 @@
 
-import React, {useState} from 'react';
-import {SafeAreaView, StyleSheet, ScrollView, View, Text, Button,} from 'react-native';
+import React, { useState } from 'react';
+import { SafeAreaView, StyleSheet, ScrollView, View, Text, Button, TextInput, } from 'react-native';
 
 
 const App = () => {
-  const [outputText, setoutputText] = useState ("open up App.js");
+  // const [outputText, setoutputText] = useState("open up App.js");
+  // const [outNumber, setOutNumber] = useState(0);
+  
+  const [enteredGoal, setEnteredGoal] = useState("");
+  const [courseGoals, setCourseGoals] = useState([])
+
+  const goalInputHandler = (enteredText) => {
+    setEnteredGoal(enteredText);
+  };
+
+  const addGoalHandler = () => {
+    setCourseGoals(currentGoals=> [...currentGoals, enteredGoal]); 
+    // console.log(enteredGoal);
+  };
 
   return (
+
     <>
-      <SafeAreaView>
-        <Text>{outputText}</Text> 
-        <Button 
-          title="change Text"
-          onPress = {() => setoutputText('The text changed')}
-        />      
+      <SafeAreaView style={{ flex: 1, padding:50 }}>
+
+        <View style={{backgroundColor:'#c8b7b5'}}>
+            <TextInput 
+                style={styles.inputContainer}
+                placeholder = " Course Goals...."
+                onChangeText = {goalInputHandler}
+                value={enteredGoal}
+            />
+            <Button 
+                title="ADD"
+                onPress={addGoalHandler}
+            />
+        </View>
+
+        {/* <View style={styles.container}>
+          <Text>{outputText}</Text>
+          <Button
+            style={styles.button}
+            title="change Text"
+            onPress={() => setoutputText('The text changed')} />
+        </View> */}
+
+        {/* <View style={styles.container}>
+          <Text style={styles.numContainer}>Sayaç: {outNumber}</Text>
+          <Button
+            style={styles.button}
+            title="Sayı Artır!!"
+            onPress={() => setOutNumber(outNumber + 1)} />
+        </View> */}
+
+        <View>
+          {courseGoals.map(goal => (
+            <View key={goal} style={{backgroundColor:'#ffff6e', marginHorizontal:5}}>
+            <Text>{goal}</Text>
+            </View>
+          ))}
+        </View>
+
       </SafeAreaView>
     </>
   );
@@ -22,3 +69,28 @@ const App = () => {
 
 
 export default App;
+
+const styles = StyleSheet.create({
+  // numContainer: {
+  //   fontSize: 45,
+  //   fontWeight: 'bold',
+  //   textAlign: 'center',
+  //   justifyContent:'space-between'
+  // },
+  // container: {
+  //   backgroundColor: 'red',
+  //   padding: 8,
+  //   margin: 5,
+  //   borderRadius: 14,
+  // },
+  inputContainer: {
+    // flex:1,
+    // flexDirection:'row',
+    backgroundColor: '#b2fab4',
+    borderColor: "black", 
+    borderRadius: 15, 
+    borderWidth:5,
+    padding:5,
+    margin:5,
+  },
+})
