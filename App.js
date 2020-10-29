b2d39820b8fc3b72834395ac9311843e7b633f8c
AliFeyzi78 <zeron1g2k@gmail.com>
2020-10-29 14:39:51 +0200

    youTube

---
+++ App.js
@@ -0,0 +1,26 @@
+
+import React, {useState} from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+
+const App = () => {
+  const [] = useState ("");
+  return (
+    <>
+      <SafeAreaView>
+        
+      </SafeAreaView>
+    </>
+  );
+};
+
+
+
+export default App;
